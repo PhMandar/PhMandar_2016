dee53b7bcdec5a769d57ee9448785f61cb0faeab
PhMandar <mandar.m.phatak@gmail.com>
2016-09-15 22:35:03 +0530

    Test
    Test 1

---
+++ HelloWorldWS/com/mandy/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+First test program on GIT hub.
+
+*/
+package com.mandy.src;
+
+
+class HelloWorld{
+
+	public static void main(String... atgs){
+		System.out.println("Hello World !");
+	}
+
+}
